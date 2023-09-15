c059878d3dc3f47a11f030bdb66e7720af3c99a0
NJBOIBOT <144768572+NJBOIBOT@users.noreply.github.com>
2023-09-15 11:02:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -61,6 +61,9 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        m=await message.reply_sticker("CAACAgUAAxkBAAEKQp9k_V350d81lhd2EZx_Y7JreFRekAACDwIAAnoEuFRrY76eeSoKYTAE")
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
